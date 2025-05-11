4885aa7d4432f181d4de503a3463e0cc308bbd32
miwanoff <marina.iva.noffa@gmail.com>
2025-05-11 11:37:01 +0300

    Add moving

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 let timer;
+let time = 1000;
+let bottom = false;
+let right = false;
+let leftOffset = 0;
+let topOffset = 0;
+
+let maxOffset = 200;
+
+const box = document.getElementById("box");
 
 function moveBox() {
-   
+  box.style.left = leftOffset + "px";
+  box.style.top = topOffset + "px";
+
+  if (leftOffset > maxOffset) {
+    right = true;
+  } else if (leftOffset == 0) {
+    right = false;
+  }
+
+  if (topOffset > maxOffset) {
+    bottom = true;
+  } else if (topOffset == 0) {
+    bottom = false;
+  }
+
+  if (bottom == false && right == false) {
+    topOffset += 30;
+  }
+
+  if (bottom == true && right == false) {
+    leftOffset += 30;
+  }
+
+  if (bottom == true && right == true) {
+    topOffset -= 30;
+  }
+
+  if (bottom == false && right == true) {
+    leftOffset -= 30;
+  }
 }
+
+timer = setInterval(moveBox, time);
